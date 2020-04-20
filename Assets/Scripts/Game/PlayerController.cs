7fd58e3a271748be8687b626a2cf41a520109d54
Xwilarg <xwilarg@yahoo.fr>
2020-04-20 20:39:16 +0200

    Add player controller

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private const float _speed = 10f;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed, _rb.velocity.y, Input.GetAxis("Vertical") * _speed);
+    }
+}
