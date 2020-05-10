1515fc9966330ddea424cc206f17d099b8d424f1
Xwilarg <xwilarg@yahoo.fr>
2020-05-10 22:16:20 +0200

    Modify map generation system

--- Assets/Scripts/Map/Generation.cs
+++ Assets/Scripts/Map/Generation.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+using Chunk = System.Collections.Generic.Dictionary<UnityEngine.Vector2Int, TileInfo>;
+
 public class Generation : MonoBehaviour
 {
     [SerializeField]
     private GameObject _prefabFloor;
 
-    private Dictionary<Vector2Int, GameObject> _instantiated;
+    [SerializeField]
+    private Material _sand, _grass;
+
+    private Dictionary<Vector2Int, Chunk> _instantiated;
+
 
     private void Start()
     {
-        _instantiated = new Dictionary<Vector2Int, GameObject>();
+        _instantiated = new Dictionary<Vector2Int, Chunk>();
         for (int x = -1; x <= 1; x++)
             for (int y = -1; y <= 1; y++)
                 InstantiateChunk(new Vector2Int(x, y));
@@ -19,6 +25,16 @@ public class Generation : MonoBehaviour
     private void InstantiateChunk(Vector2Int pos)
     {
         if (!_instantiated.ContainsKey(pos))
-            _instantiated.Add(pos, Instantiate(_prefabFloor, new Vector3(pos.x * 10f, 0f, pos.y * 10f), Quaternion.identity));
+        {
+            var chunk = new Chunk();
+            for (int x = 0; x < 10; x++)
+                for (int y = 0; y < 10; y++)
+                {
+                    var go = Instantiate(_prefabFloor, new Vector3(pos.x * 10f, 0f, pos.y * 10f), Quaternion.identity);
+                    go.GetComponent<MeshRenderer>().material = _sand;
+                    chunk.Add(new Vector2Int(x, y), new TileInfo(go));
+                }
+            _instantiated.Add(pos, chunk);
+        }
     }
 }
---
+++ Assets/Scripts/Map/TileInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TileInfo
+{
+    public TileInfo(GameObject gameObject)
+    {
+        _gameObject = gameObject;
+        _type = TileType.Sand;
+    }
+
+    private GameObject _gameObject;
+    private TileType _type;
+}
