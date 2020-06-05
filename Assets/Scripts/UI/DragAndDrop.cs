93b3fcd578336f011a8d3b11b6408514f11c11c1
Xwilarg <xwilarg@yahoo.fr>
2020-06-05 15:33:19 +0200

    Add ability to drag and drop console

---
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+
+	private Vector2 pointerOffset;
+	private RectTransform canvasRectTransform;
+	private RectTransform panelRectTransform;
+
+	void Start()
+	{
+		Canvas canvas = GetComponentInParent<Canvas>();
+		canvasRectTransform = (RectTransform)canvas.transform;
+		panelRectTransform = (RectTransform)transform;
+	}
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		panelRectTransform.SetAsLastSibling();
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+	}
+
+	public void OnDrag(PointerEventData data)
+	{
+		if (panelRectTransform == null)
+			return;
+
+		Vector2 pointerPosition = data.position;
+		Vector2 localPointerPosition;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, pointerPosition, data.pressEventCamera, out localPointerPosition))
+		{
+			panelRectTransform.localPosition = localPointerPosition - pointerOffset;
+		}
+	}
+}
