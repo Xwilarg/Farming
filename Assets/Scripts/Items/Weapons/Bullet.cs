2e975df3467df5b78922e8166bdbc7a9c658079e
Xwilarg <xwilarg@yahoo.fr>
2020-06-06 22:05:11 +0200

    Add gun impact

--- Assets/Scripts/Items/Weapons/Bullet.cs
+++ Assets/Scripts/Items/Weapons/Bullet.cs
@@ -2,8 +2,12 @@
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject gunImpact;
+
     private void OnCollisionEnter(Collision collision)
     {
+        Destroy(Instantiate(gunImpact, collision.contacts[0].point + (Vector3.up * 0.001f), Quaternion.identity), 3f);
         Destroy(gameObject);
     }
 }
