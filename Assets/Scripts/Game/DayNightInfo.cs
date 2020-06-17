8efdef0853adba8bd55ace978042a3c68bb9b1d8
Xwilarg <xwilarg@yahoo.fr>
2020-06-17 12:51:57 +0200

    Begin to work on day/night management

---
+++ Assets/Scripts/Game/DayNightInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/DayNightInfo", fileName = "DayNightInfo")]
+public class DayNightInfo : ScriptableObject
+{
+    [Tooltip("Length of the day in minute")]
+    public float dayLength;
+
+    [Tooltip("Length of the night in minute")]
+    public float nightLength;
+}
---
+++ Assets/Scripts/Game/DayNightManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class DayNightManager : MonoBehaviour
+{
+    [SerializeField]
+    private Light _sun;
+
+    [SerializeField]
+    private DayNightInfo _dayNightInfo;
+
+    private float _timer;
+    private DayNightState _state;
+    private float _baseIntensity, _nextIntensity;
+
+    private void Start()
+    {
+        _state = DayNightState.AfterNoon; // We start the day at noon
+        _timer = (_dayNightInfo.dayLength * 60f) / 2f; // Remaining timer is only half a day
+        _baseIntensity = 1;
+        _nextIntensity = 0;
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0f)
+        {
+            switch (_state)
+            {
+                case DayNightState.AfterNoon:
+                case DayNightState.BeforeNoon:
+                    _timer = (_dayNightInfo.dayLength * 60f) / 2f;
+                    break;
+
+                case DayNightState.BeforeNight:
+                    _timer = _dayNightInfo.nightLength * 60f;
+                    break;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public enum DayNightState
+    {
+        BeforeNoon,
+        AfterNoon,
+        BeforeNight
+    }
+}
